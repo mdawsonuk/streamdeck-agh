3c0a2b8186edebd4695bcbb0465aeca048a50d8d
Matt <matthewd102@hotmail.com>
2022-03-30 00:11:32 +0100

    Added global settings so that authentication settings persist
    Closes #2

--- dev.mdawson.agh.sdPlugin/inspector/inspector.js
+++ dev.mdawson.agh.sdPlugin/inspector/inspector.js
@@ -1,6 +1,7 @@
 var websocket = null;
 var action = null;
 var context = null;
+var globalSettings = {};
 
 // Send data over the Websocket to the Elgato Stream Deck software
 function send(data) {
@@ -15,12 +16,27 @@ function connectElgatoStreamDeckSocket(inPort, inPropertyInspectorUUID, inRegist
             "event": inRegisterEvent,
             "uuid": inPropertyInspectorUUID
         });
-    }
+        send({
+            "event": "getGlobalSettings",
+            "context": inPropertyInspectorUUID
+        });
+    };
 
     websocket.onmessage = function (evt) {
-        jsonObj = json.parse(evt.data);
+        jsonObj = JSON.parse(evt.data);
         let event = jsonObj.event;
-    }
+        let jsonPayload = jsonObj.payload;
+
+        if (event === 'didReceiveGlobalSettings') {
+            // Set global plugin settings
+            globalSettings = jsonPayload.settings;
+
+            document.querySelector("#agh-url-input").value = globalSettings.agh_url ? globalSettings.agh_url : "";
+            document.querySelector("#agh-https-input").checked = globalSettings.agh_https ? globalSettings.agh_https : false;
+            document.querySelector("#agh-username-input").value = globalSettings.agh_username ? globalSettings.agh_username : "";
+            document.querySelector("#agh-password-input").value = globalSettings.agh_password ? globalSettings.agh_password : "";
+        }
+    };
 
     let actionInfo = JSON.parse(inActionInfo);
     action = actionInfo.action;
@@ -28,34 +44,59 @@ function connectElgatoStreamDeckSocket(inPort, inPropertyInspectorUUID, inRegist
 
     let settings = actionInfo.payload.settings;
     document.querySelector("#agh-info-input").value = settings.agh_info ? settings.agh_info : "none";
-    document.querySelector("#agh-url-input").value = settings.agh_url ? settings.agh_url : "";
-    document.querySelector("#agh-username-input").value = settings.agh_username ? settings.agh_username : "";
-    document.querySelector("#agh-password-input").value = settings.agh_password ? settings.agh_password : "";
 }
 
 function storeSettings() {
     let info = document.querySelector("#agh-info-input").value;
-    let url = document.querySelector("#agh-url-input").value;
-    let https = document.querySelector("#agh-https-input").checked;
-    let username = document.querySelector("#agh-username-input").value;
-    let password = document.querySelector("#agh-password-input").value;
     send({
         "event": "setSettings",
         "context": context,
         "payload": {
             "agh_info": info,
-            "agh_url": url,
-            "agh_https": https,
-            "agh_username": username,
-            "agh_password": password
         }
     });
 }
 
+function storeGlobalSettings() {
+    globalSettings.agh_url = document.querySelector("#agh-url-input").value;
+    globalSettings.agh_https = document.querySelector("#agh-https-input").checked;
+    globalSettings.agh_username = document.querySelector("#agh-username-input").value;
+    globalSettings.agh_password = document.querySelector("#agh-password-input").value;
+    send({
+        "event": "setGlobalSettings",
+        "context": context,
+        "payload": {
+            "agh_url": globalSettings.agh_url,
+            "agh_https": globalSettings.agh_https,
+            "agh_username": globalSettings.agh_username,
+            "agh_password": globalSettings.agh_password
+        }
+    });
+}
+
+// Log to the global log file
+function log(inMessage) {
+    // Log to the developer console
+    let time = new Date();
+    let timeString = time.toLocaleDateString() + ' ' + time.toLocaleTimeString();
+    console.log(timeString, inMessage);
+
+    // Log to the Stream Deck log file
+    if (websocket) {
+        websocket.send(JSON.stringify({
+            event: 'logMessage',
+            payload: {
+                message: inMessage,
+            },
+        }));
+    }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelector("#agh-info-input").onchange = storeSettings;
-    document.querySelector("#agh-url-input").onchange = storeSettings;
-    document.querySelector("#agh-https-input").onchange = storeSettings;
-    document.querySelector("#agh-username-input").onchange = storeSettings;
-    document.querySelector("#agh-password-input").onchange = storeSettings;
+
+    document.querySelector("#agh-url-input").onchange = storeGlobalSettings;
+    document.querySelector("#agh-https-input").onchange = storeGlobalSettings;
+    document.querySelector("#agh-username-input").onchange = storeGlobalSettings;
+    document.querySelector("#agh-password-input").onchange = storeGlobalSettings;
 });
