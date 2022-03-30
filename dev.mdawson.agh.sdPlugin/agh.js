1ea6352603afc2808a2bffc78dd3ce118d3aaf95
Matt <matthewd102@hotmail.com>
2022-03-30 18:00:14 +0100

    Stats API calls in AGH wrapper

--- dev.mdawson.agh.sdPlugin/agh.js
+++ dev.mdawson.agh.sdPlugin/agh.js
@@ -8,10 +8,64 @@ class AdGuardHomeAPI {
     }
 
     testConnection() {
-        this.request("status");
+        this.request("status", result => {
+            if (result.status === 200) {
+                alert("Successfully connected to AdGuard Home API");
+            } else if (result.status === 403) {
+                alert("Error connecting to AdGuard Home API: Username or Password incorrect");
+            }
+        });
     }
 
-    request(endpoint) {
+    //#region Stats
+
+    getDnsQueryCount(callback) {
+        this.statsRequest("num_dns_queries", d => callback(d));
+    }
+
+    getBlockPercentage(callback) {
+        this.statsRequest("", data => {
+            // Avoid dividing by zero
+            if (data.num_dns_queries === 0) {
+                callback(0);
+            }
+            callback((data.num_blocked_filtering / data.num_dns_queries * 100).toFixed(2));
+        });
+    }
+
+    getFilterBlockCount(callback) {
+        this.statsRequest("num_blocked_filtering", d => callback(d));
+    }
+
+    getSafeBrowsingCount(callback) {
+        this.statsRequest("num_replaced_safebrowsing", d => callback(d));
+    }
+
+    getSafeSearchCount(callback) {
+        this.statsRequest("num_replaced_safesearch", d => callback(d));
+    }
+
+    getParentalFilterCount(callback) {
+        this.statsRequest("num_replaced_parental", d => callback(d));
+    }
+
+    getAverageProcessingTime(callback) {
+        this.statsRequest("avg_processing_time", d => callback(d * 1000));
+    }
+
+    statsRequest(stat, callback) {
+        this.request("stats", result => {
+            if (stat !== "") {
+                callback(result.data[stat]);
+            } else {
+                callback(result.data);
+            }
+        });
+    }
+
+    //#endregion
+
+    request(endpoint, callback) {
         var proto = this.https ? "https://" : "http://";
 
         var url = proto + this.url + "/control/" + endpoint;
@@ -23,13 +77,10 @@ class AdGuardHomeAPI {
 
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4) {
-                console.log(xhr.status);
-                console.log(xhr.responseText);
-                if (xhr.status === 200) {
-                    alert("Successfully connected to AdGuard Home API");
-                } else if (xhr.status === 403) {
-                    alert("Error connecting to AdGuard Home API: Username or Password incorrect");
-                }
+                callback({
+                    status: xhr.status,
+                    data: JSON.parse(xhr.response)
+                })
             }
         };
 
